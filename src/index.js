92f3ed03b86aedc371e6b5832901d14b9ffeec9a
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-14 07:55:40 -0300

    Nuevas funciones
    Nuevos métodos para las entregas y deudas, y uno para cancelar las deudas

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ const express = require("express")
 const cors = require("cors")
 const app = express();
 const multer = require("multer");
-const { Query } = require("pg");
+const cron = require("node-cron")
+const dayjs = require("dayjs")
+const utc = require("dayjs/plugin/utc.js")
+const timezone = require("dayjs/plugin/timezone.js")
 
+dayjs.extend(utc)
+dayjs.extend(timezone)
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
@@ -269,11 +274,176 @@ app.post("/save-client-debt/:id", upload.none(), async(req,res)=> {
     }
 })
 
+app.post("/save-client-deliver/:id",upload.none(), async(req,res)=> {
+    const { id } = req.params
+    const { deliversData } = req.body
+    if (!deliversData) {
+        return res.status(400).json({message: "No se pudo guardar la entrega, faltan datos obligatorios!"})
+    }
+    const insertQuery = `INSERT INTO entregas(detalle_entrega, id_entrega_cliente) VALUES ($1, $2)`
+    const client = await clientDb.connect()
+    try {
+        const result = await client.query(insertQuery,[deliversData, id])
+        if (result.rowCount > 0) {
+            return res.status(200).json({message: "Entrega guardada!"})
+        }
+        return res.status(400).json({message: "Error al guardar la entrega, por favor intente nuevamente"})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message: "Error de servidor, no se pudo guardar la entrega"})
+    }finally{
+        client.release
+    }
+})
 
+app.put("/UPDATE-client-deliver/:id",upload.none(), async(req,res)=> {
+    const { id } = req.params
+    const { deliversData } = req.body
+    if (!deliversData) {
+        return res.status(400).json({message: "No se pudo guardar la entrega, faltan datos obligatorios!"})
+    }
+    const insertQuery = `UPDATE entregas SET detalle_entrega = $1 WHERE id = $2`
+    const client = await clientDb.connect()
+    try {
+        const result = await client.query(insertQuery,[deliversData, id])
+        if (result.rowCount > 0) {
+            return res.status(200).json({message: "Entrega guardada!"})
+        }
+        return res.status(400).json({message: "Error al guardar la entrega, por favor intente nuevamente"})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message: "Error de servidor, no se pudo guardar la entrega"})
+    }finally{
+        client.release()
+    }
+});
 
+app.delete("/delete-client-deliver/:id", async(req,res) => {
+    const { id } = req.params
+    const query = `DELETE FROM entregas WHERE id = $1`
+    const client = await clientDb.connect()
+    try {
+        if (!id) {
+            throw new Error("No se pudo obtener el ID de la entrega, intente nuevamente!")
+        }
 
+        const result = await client.query(query,[id])
+        if (!result.rowCount > 0) throw new Error("Error eliminando la entrega, intente nuevamente!")
+        return res.status(200).json({message: "Entrega eliminada exitosamente"})
 
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message: error || "Error interno del servidor al eliminar la entrega, intente nuevamente!"})
+    }finally{
+        client.release()
+    }
+})
 
+app.put("/update-client-debt/:clientId", upload.none(), async(req,res)=> {
+    const { clientId } = req.params
+    const { productos, buyDate, expDate, clientDebtId } = req.body
+
+    const updateQuery = `UPDATE deudas SET fecha_compra = $1, fecha_vencimiento = $2, detalles = $3 WHERE deuda_uuid = $4`
+    const client = await clientDb.connect()
+    try {
+        if (!productos || !buyDate || !expDate || !clientDebtId) {
+            throw new Error("No se pudo actualizar la deuda, faltan datos obligatorios!")
+        }
+        const result = await client.query(updateQuery,[buyDate, expDate, productos, clientDebtId])
+        if (result.rowCount === 0) throw new Error("Error al actualizar la deuda, intente nuevamente!")
+        return res.status(200).json({message: "Deuda actualizada"})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message: error || "Error interno del servidor al actualizar la deuda, intente nuevamente!"})
+    }finally{
+        client.release()
+    }
+})
+
+app.delete("/delete-client-debt/:debtId", async(req,res)=> {
+    const { debtId } = req.params
+    const query = `DELETE FROM deudas WHERE deuda_uuid = $1`
+    const client = await clientDb.connect()
+    try {
+        if(!debtId) throw new Error("No se proporcionó el ID de la deuda, intente nuevamente!")
+        const result = await client.query(query,[debtId])
+        if (result.rowCount === 0) throw new Error("Error al eliminar la deuda, intente nuevamente!")
+        return res.status(200).json({message: "Deuda eliminada exitosamente"})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message: error || "Error interno del servidor al eliminar la deuda, intente nuevamente!"})
+    }finally{
+        client.release()
+    }
+});
+
+cron.schedule("0 0 * * *", async() => {
+    const client = await clientDb.connect()
+    const argentinaTime = dayjs().tz("America/Buenos_Aires").format("YYYY-MM-DD")
+    const query = `UPDATE deudas SET estado = false WHERE fecha_vencimiento <= $1 RETURNING *`
+    try {
+        console.log("EJECUTANDO TAREAS CRON")
+        const response = await client.query(query,[argentinaTime])
+        if (response.rowCount > 0) {
+            console.log(`Vencimiento de deudas actualizadas exitosamente!, ${response.rowCount} deudas actualizadas`)
+        }else{
+            console.log("Ninguna deuda vencida!")
+
+        }
+    } catch (error) {
+        console.log("Error ejecutando la tarea CRON", error)
+    }finally{
+        console.log("TAREAS CRON FINALIZADAS!")
+        client.release()
+    }
+});
+
+app.post("/cancel-client-debts/:clientId", async(req,res)=> {
+    const { clientId } = req.params
+    const client = await clientDb.connect()
+    const query1 = `SELECT * FROM deudas WHERE cliente_id = $1`
+    const query2 = `SELECT* FROM entregas WHERE id_entrega_cliente = $1`
+    const query3 = `INSERT INTO historial_deudas(historial_cliente_id, detalle_entregas, detalle_deudas, fecha_cancelacion) VALUES ($1, $2, $3, $4)`
+    const query4 = `DELETE FROM deudas WHERE cliente_id = $1`
+    const query5 = "DELETE FROM entregas WHERE id_entrega_cliente = $1"
+
+    if (!clientId) {
+        return res.status(404).json({message: "No se pudo cancelar la deuda, intente nuevamente!"})
+    }
+
+    try {
+        await client.query("BEGIN")
+        const [debts, deliveries] = await Promise.all([
+            client.query(query1,[clientId]),
+            client.query(query2,[clientId])
+        ])
+
+        if (debts.rowCount === 0 || deliveries.rowCount === 0) throw new Error("El cliente no tiene deudas o entregas para poder hacer una cancelación, intente nuevamente más tarde!")
+        const serializeDebts = JSON.stringify(debts.rows)
+        const serializeDeliveries = JSON.stringify(deliveries.rows)
+
+        const result = await client.query(query3,[clientId, serializeDebts, serializeDeliveries, dayjs().format("YYYY-MM-DD HH:mm:ss")])
+        if (result.rowCount === 0) throw new Error("Error cancelando la deuda, intente nuevamente!")
+        const result2 = await Promise.all([
+            client.query(query4,[clientId]),
+            client.query(query5,[clientId])
+        ])
+
+        if (result2[0].rowCount === 0 || result2[1].rowCount === 0) throw new Error("Error cancelando la deuda, intente nuevamente!")
+        await client.query("COMMIT")
+        return res.status(200).json({message: "Deuda cancelada exitosamente!"})
+    } catch (error) {
+        console.log(error)
+        await client.query("ROLLBACK")
+        return res.status(500).json({message: error || "Error interno del servidor al cancelar la deuda, intente nuevamente!"})
+    }finally{
+        client.release()
+    }
+})
+
+app.put("/update-debt-status/:", async(req,res)=> {
+
+})
 
 
 
